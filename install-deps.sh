4a8bec8ded2af1110fcd65fcd5a0944c66f6b6c5
james-mcalea <jamesmca4713@gmail.com>
2024-05-16 20:19:21 +0100

    Install deps script

---
+++ install-deps.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install libpq-dev
+
+pip install uvicorn
+pip install sentry-sdk
+pip install sqlmodel
+pip install passlib
+pip install pydantic
+pip install pydantic_settings
+pip install psycopg
+pip install emails
